fd2db4fd2e0559058ee2306f899e105576499819
LimChetra <limchetra60@gmail.com>
2024-01-10 20:10:50 +0700

    midterm java

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Square square = new Square(5.0);
+        System.out.println("Area of square: " + square.getArea());
+        System.out.println("Perimeter of square: " + square.getPerimeter());
+    }
+}
